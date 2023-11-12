8be28c31c737c6cdbe121aba61eb2975aa0160a6
Ferreto (Arashi Projects) <ferreto@kunoichi.be>
2023-11-12 17:18:25 +0100

    Create config.h

---
+++ ESP32_OLED_NTPclock/config.h
@@ -0,0 +1,12 @@
+// Set to true to enable serial monitor
+#define USE_SERIAL false
+
+// WIFI
+const char* wifi_ssid = "<WiFi SSID here>";   // WiFi SSID
+const char* wifi_pwd = "<WiFi password here>";    // WiFi password
+
+
+// NTP
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 3600;       // GMT +/- in seconds
+const int   daylightOffset_sec = 0;  // Daylight savings 0|3600
